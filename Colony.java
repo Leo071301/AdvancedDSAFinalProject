95c91f2c0b7e9cd7c990818e49283808d68efea8
dianarcesar <dianarcesar@gmail.com>
2026-02-10 17:33:13 -0600

    Create Colony.java

---
+++ Colony.java
@@ -0,0 +1,55 @@
+import java.awt.geom.Point2D;
+import java.util.Map;
+
+public class Colony implements Identifiable {
+    private final long colony_id;
+    private String name;
+    private final Point2D location;
+    private int riskFactor; // 1-5: 1 safest and 5 the most dangerous
+    private HashMap<String, Resource> inventory;
+
+    public Colony(long colony_id, String name, Point2D location, int riskFactor) {
+        if (riskFactor < 1 || riskFactor > 5) {
+            throw new IllegalArgumentException("riskFactor must be between 1 and 5");
+        }
+        this.colony_id = colony_id;
+        this.location = location;
+        this.name = name;
+        this.riskFactor = riskFactor;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Point2D getLocation() {
+        return location;
+    }
+
+    public int getRiskFactor() {
+        return riskFactor;
+    }
+
+    public void setRiskFactor(int riskFactor) {
+        this.riskFactor = riskFactor;
+    }
+
+    // TODO: add resources to the inventory
+    public void addResource(String key, int amount) {}
+
+    // TODO: remove resources from the inventory
+    public void removeResource(String key, int amount) {}
+
+    // TODO: check whether the colony's inventory contains a resource and how much of it
+    public boolean hasResource(String key, int amount) { return false; }
+
+    // TODO: return a Map of inventory (view-only, use Collections.unmodifiable)
+    public Map viewInventory() { return Map.of(); }
+
+    // TODO: create a trade request asking for a specific resource and the amount
+    public TradeRequest createTradeRequest(String key, int amount) { return TradeRequest; }
+}
