7814c4f8e43d6069975779aea2d93c80e2a20d0a
dianarcesar <dianarcesar@gmail.com>
2026-02-11 19:11:45 -0600

    Update Resource.java

--- Resource.java
+++ Resource.java
@@ -1,32 +1,25 @@
-public abstract class Resource {
+public class Food extends Resource {
 
-    private String name;
-    private int amount;
+    private String type; // e.g., "Pills", "Canned", etc.
 
-    public Resource(String name, int amount){
-        this.name = name;
-        this.amount = amount;
+    // Trade request constructor
+    public Food(String name, int amount, String type) {
+        super(name, amount);
+        this.type = type;
     }
 
-    public int getAmount(){ return amount; }
-
-    public String getName(){ return name; }
-
-    public abstract Resource copy();
-
-    // Add resources to resource amount
-    public void addAmount(int amount) {
-        if (amount < 0) {
-            throw new IllegalArgumentException("Cannot add negative amount");
-        }
-        this.amount += amount;
+    // Copy constructor (for inventory copy)
+    public Food(Food other) {
+        super(other.getName(), other.getAmount());
+        this.type = other.type;
     }
 
-    // Remove resources from resource amount
-    public void removeAmount(int amount) {
-        if (amount < 0) {
-            throw new IllegalArgumentException("Cannot remove negative amount");
-        }
-        this.amount -= amount;
+    @Override
+    public Resource copy() {
+        return new Food(this); // uses copy constructor
+    }
+
+    public String getType() {
+        return type;
     }
 }
