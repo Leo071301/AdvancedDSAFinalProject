a9f777dd289f1044b38de4892841d08dd47d4063
Leonel Avellaneda <55268634+Leo071301@users.noreply.github.com>
2026-04-21 17:59:23 -0500

    Delete ResourceComparator.java

--- ResourceComparator.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-public class ResourceComparator implements Comparator<Resource> {
-    @Override
-    public int compare(Resource r1, Resource r2) {
-        // compares resources by amount
-        return Integer.compare(r1.getAmount(), r2.getAmount());
-    }
-}
